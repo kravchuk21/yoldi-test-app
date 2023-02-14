de27df36b23a3fa88af04b839be73afb804382e8
kravchuk21 <krauchukvlad@gmail.com>
2023-02-14 17:01:30 +0300

    UserCard component

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,68 @@
 "use client"
 
-import Avatar from "@/components/Avatar";
-import Button from "@/components/Button";
-import Input from "@/components/Input";
-import Typography from "@/components/Typography";
-import React from "react";
-import Svg from "@/public/assets/icons/user.svg"
-import Modal from "@/components/Modal";
+import Title from "@/components/Title"
+import UserCard from "@/components/UserCard"
+
+const DATA = [
+  {
+    "name": "Владислав Кравчук",
+    "email": "krauchukvlad@gmail.com",
+    "slug": "string",
+    "description": "string", 
+    "cover": {
+      "id": "string",
+      "url": "string",
+      "width": "string",
+      "height": "string"
+    }
+  },
+  {
+    "name": "string",
+    "email": "string",
+    "slug": "string",
+    "description": "string", 
+    "cover": {
+      "id": "string",
+      "url": "string",
+      "width": "string",
+      "height": "string"
+    }
+  },
+  {
+    "name": "string",
+    "email": "string",
+    "slug": "string",
+    "description": "string", 
+    "cover": {
+      "id": "string",
+      "url": "string",
+      "width": "string",
+      "height": "string"
+    }
+  },
+  {
+    "name": "string",
+    "email": "string",
+    "slug": "string",
+    "description": "string", 
+    "cover": {
+      "id": "string",
+      "url": "string",
+      "width": "string",
+      "height": "string"
+    }
+  },
+]
 
 export default function Home() {
-	const [value, setValue] = React.useState('');
-	const [visible, setVisble] = React.useState(false)
 	return (
 		<div>
-				<h1>Yoldi test app</h1>
-				<Typography color="red">Привет</Typography>
-				<Button onClick={() => setVisble(true)}>Open</Button>
-				<Modal isOpened={visible} onClose={() => setVisble(false)}>
-					hello
-					<Button onClick={() => setVisble(false)} buttonType="primary">Close</Button>
-				</Modal>
-				<input value={value} onChange={e => setValue(e.target.value)}/>
-				<Button>hello</Button>
-				<Button disabled>hello</Button>
-				<Button buttonType="primary">hello</Button>
-				<Button buttonType="primary" disabled>hello</Button>
-				<Button RightIcon={Svg} LeftIcon={Svg}>hello</Button>
-				<Input fullWidth RightIcon={Svg} LeftIcon={Svg} error placeholder="hello"/>
-				<Avatar fullName="Владислав" size="small" image={null}/>
-				<Avatar fullName="Владислав" image={"https://yoldi.agency/static/assets/yoldi-social.jpg"}/>
-				<Avatar fullName="Владислав" size="large" image={"https://yoldi.agency/static/assets/yoldi-social.jpg"}/>
-				<Avatar fullName="Владислав" size="large" image={null}/>
+			<Title>Список аккаунтов</Title>
+			{DATA.map(user => {
+				return (
+					<UserCard fullName={user.name} email={user.email} image={null} key={user.email}/>
+				)
+			})}
 		</div>
 	)
 }
